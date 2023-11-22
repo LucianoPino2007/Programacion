0f382056043c3d7e6686890df2801a3c2a74d1c2
LucianoPino2007 <115808799+LucianoPino2007@users.noreply.github.com>
2023-11-22 10:48:36 -0300

    Update poo.py

--- poo.py
+++ poo.py
@@ -3,5 +3,14 @@ class Rectangulo:
         self.longitud = longitud
         self.ancho = ancho
         
-
+    def calcular_Area(self):
+        self.area=self.longitud * self.ancho
+        return self.area
     
+    def calcular_Perimetro(self):
+        self.perimetro= (self.longitud + self.ancho) * 2
+        return self.perimetro
+obj1 = Rectangulo(5,10)
+    
+print("Area del rectangulo", obj1.calcular_Area())
+print("Perimetro del rectangulo: ", obj1.calcular_Perimetro())
